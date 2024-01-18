651497218f702964db36ea459640560187a5cec6
Laura Jaimes <146447166+geolaurajaimes@users.noreply.github.com>
2024-01-18 15:08:27 -0500

    Update clustering.js

--- clustering/clustering.js
+++ clustering/clustering.js
@@ -1,6 +1,6 @@
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 3,
+    zoom: 6,
     center: { lat: 46.944168, lng: -84.551017 },
   });
   // Create an array of alphabetical characters used to label the markers.
